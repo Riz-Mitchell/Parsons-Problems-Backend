4fa9aeacda7653e732a714bd9d3b5a5cd2204781
Riz-Mitchell <rileymitch117@gmail.com>
2024-09-07 22:39:46 +1000

    fixed verifyUser function

--- middleware/verifyUser.js
+++ middleware/verifyUser.js
@@ -1,13 +1,8 @@
 const verifyUser = (req, res, next) => {
     const id = req.query.id;
-    console.log(`outside thing: ${id}`);
     
     // Check if url params id = the JWT user id
-    if (req.sub !== id) {
-        console.log(`${req.sub}`);
-        console.log(`${id}`);
-
-        console.log('cman420');
+    if (String(req.sub) !== id) {
         // Potential random XSS attack :(
         return res.sendStatus(403);
     }
