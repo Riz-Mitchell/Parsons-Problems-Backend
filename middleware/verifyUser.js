a02ff342a2f76508aa82cf3f4bf417b5661daba7
Riz-Mitchell <rileymitch117@gmail.com>
2024-09-07 23:18:02 +1000

    Fixed type issues

--- middleware/verifyUser.js
+++ middleware/verifyUser.js
@@ -2,7 +2,7 @@ const verifyUser = (req, res, next) => {
     const id = req.query.id;
     
     // Check if url params id = the JWT user id
-    if (String(req.sub) !== id) {
+    if (req.sub !== id) {
         // Potential random XSS attack :(
         return res.sendStatus(403);
     }
