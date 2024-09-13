e01521bdff999332a417dd697474bd156654f855
Riz-Mitchell <rileymitch117@gmail.com>
2024-09-13 17:03:40 +1000

    Deleted temp function

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const mongoose = require('mongoose');
 const config = require('./config/config.js');
 const authRoutes = require('./routes/authRoutes.js');
 const userRoutes = require('./routes/userRoutes.js');
-const APIRoutes = require('./helpers/geminiInterface.js');
 const testRoutes = require('./routes/testRoutes.js');
-const feedbackRoutes = require('./routes/feedbackRoutes.js');
 const parsonsProblemsRoutes = require('./routes/parsonsProblemsRoutes.js');
 const cookieParser = require('cookie-parser');
 const { spawn } = require('child_process');
@@ -31,28 +29,7 @@ app.use('/api/test', requestHandler, testRoutes);
 
 app.use('/api/users', requestHandler, userRoutes);
 app.use('/api/parsonProblem', requestHandler, parsonsProblemsRoutes);
-app.use('/api/feedback', feedbackRoutes);
 
-app.get('/run-python', (req, res) => {
-    const pythonProcess = spawn('python3', ['./python/script.py']);
-    try {
-        pythonProcess.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
-            res.json(JSON.parse(data));
-        });
-    
-        pythonProcess.stderr.on('data', (data) => {
-            console.error(`stderr: ${data}`);
-            res.status(500).send('Error running Python script');
-        });
-    
-        pythonProcess.on('close', (code) => {
-            console.log(`Python script exited with code ${code}`);
-        });
-    } catch (error) {
-        console.log(error);
-    }
-});
 
 mongoose.connect(config.db.uri, config.db.options)
     .then(() => {
